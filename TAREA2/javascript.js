0781d45ff12f3cc5a343b12cebdb1a910bf61647
BelenLarriel <159143086+BelenLarriel@users.noreply.github.com>
2024-02-20 13:37:49 -0300

    Add files via upload

---
+++ TAREA2/javascript.js
@@ -0,0 +1,54 @@
+function verArchivos(targetId) {
+    // Toggle de la visibilidad del colapso
+    $(targetId).collapse('toggle');
+
+    // Toggle de la visibilidad del botón de versión
+    $('#versionButton1').toggle();
+}
+
+
+
+
+    
+function verDetalles(targetId) {
+    // Toggle de la visibilidad del colapso
+    $(targetId).collapse('toggle');
+
+    $('#versionButton2').toggle();
+}
+
+    
+function verConfiguracion(targetId) {
+    // Toggle de la visibilidad del colapso
+    $(targetId).collapse('toggle');
+
+    $('#versionButton3').toggle();
+}
+
+    
+
+
+function verArchivosdos(targetId) {
+    // Toggle de la visibilidad del colapso
+    $(targetId).collapse('toggle');
+
+    $('#versionButton4').toggle();
+}
+
+    
+function verDetallesdos(targetId) {
+    // Toggle de la visibilidad del colapso
+    $(targetId).collapse('toggle');
+
+    $('#versionButton5').toggle();
+}
+
+
+function verconfiguraciondos(targetId) {
+
+    $(targetId).collapse('toggle');
+
+    $('#versionButton6').toggle();
+
+
+}
